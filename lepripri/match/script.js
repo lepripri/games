0be860b9aa9bad221baee12adc607c87594c5ef5
Camille <camille.melinjulien@hotmail.com>
2025-12-18 18:04:45 +0100

    Update script.js

--- lepripri/match/script.js
+++ lepripri/match/script.js
@@ -12,12 +12,14 @@ if (localStorage.games == undefined) {
     localStorage.setItem('games', '{"favoriteGames": ""}')
 }
 var gamesStorage = JSON.parse(localStorage.games);
-var lastGamesStorage = gamesStorage;
+let lastGamesStorage = JSON.stringify(gamesStorage);
+
 setInterval(() => {
-    if (lastGamesStorage != gamesStorage) {
-        localStorage.setItem('games', JSON.stringify(gamesStorage));
-        var gamesStorage = JSON.parse(localStorage.games);
-        var lastGamesStorage = gamesStorage;
+    const currentGames = JSON.stringify(gamesStorage);
+
+    if (currentGames !== lastGamesStorage) {
+        localStorage.setItem('games', currentGames);
+        lastGamesStorage = currentGames;
     }
 }, 100);
 createObject(4, 4, "RDP01");

960d64778c4e4d866ab2561d9092b754f7a75046
Camille <camille.melinjulien@hotmail.com>
2025-12-19 17:57:43 +0100

    Update script.js

--- lepripri/match/script.js
+++ lepripri/match/script.js
@@ -1,30 +1,3 @@
-options.onchange = () => {
-    options.value = "options";
-};
-function createObject (ln, col, objectID) {
-    grid.querySelector(`.c${col}-${ln}`).setAttribute('completed', '')
-    var newElement = grid.querySelector(`.c${col}-${ln}`).appendChild(document.createElement('img'))
-    newElement.src = "icons/" + objectID + ".png"
-    newElement.draggable = false;
-    return newElement;
-}
-if (localStorage.games == undefined) {
-    localStorage.setItem('games', '{"favoriteGames": ""}')
-}
-var gamesStorage = JSON.parse(localStorage.games);
-let lastGamesStorage = JSON.stringify(gamesStorage);
-
-setInterval(() => {
-    const currentGames = JSON.stringify(gamesStorage);
-
-    if (currentGames !== lastGamesStorage) {
-        localStorage.setItem('games', currentGames);
-        lastGamesStorage = currentGames;
-    }
-}, 100);
-createObject(4, 4, "RDP01");
-createObject(4, 5, "RDP01");
-
 var preInitiation =  {
         energy: {
             level: 100,
@@ -40,7 +13,7 @@ var preInitiation =  {
         grid: [
             [
                  {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false},
-                 {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false},
+                 {filled: false}, {filled: false}, {filled: false}, {filled: true, id: "PPC02", locked: true, boxed: {trued: true, level: 0}}, {filled: true, id: "RDP03", locked: true, boxed: {trued: true, level: 2}}, {filled: true, id: "RDP02", locked: true, boxed: {trued: true, level: 2}}, {filled: true, id: "RDP02", locked: true, boxed: {trued: true, level: 2}},
                  {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false},
                  {filled: false}, {filled: false}, {filled: false}, {filled: true, id: "RDP01", locked: false, boxed: {trued: false, level: 0}, product: {items: [], energyConsomation: true, possibility: false}}, {filled: true, id: "RDP01", locked: false, boxed: {trued: false, level: 0}, product: {items: [], energyConsomation: true, possibility: false}}, {filled: false}, {filled: false},
                  {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false}, {filled: false}, 
@@ -79,3 +52,29 @@ var preInitiation =  {
             maxleight: 10
         }
     };
+options.onchange = () => {
+    options.value = "options";
+};
+function createObject (ln, col, objectID) {
+    grid.querySelector(`.c${col}-${ln}`).setAttribute('completed', '')
+    var newElement = grid.querySelector(`.c${col}-${ln}`).appendChild(document.createElement('img'))
+    newElement.src = "icons/" + objectID + ".png"
+    newElement.draggable = false;
+    return newElement;
+}
+if (localStorage.games == undefined) {
+    localStorage.setItem('games', '{"favoriteGames": ""}')
+}
+var gamesStorage = JSON.parse(localStorage.games);
+let lastGamesStorage = JSON.stringify(gamesStorage);
+if (gameStrorage.match == undefined) {
+    gameStrorage.match = preInitiation;
+}
+setInterval(() => {
+    const currentGames = JSON.stringify(gamesStorage);
+
+    if (currentGames !== lastGamesStorage) {
+        localStorage.setItem('games', currentGames);
+        lastGamesStorage = currentGames;
+    }
+}, 100);
